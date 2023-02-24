ee79f96475c265fc28fc16fe298ee89629921d4e
Tkampus <125730373+tkampus@users.noreply.github.com>
2023-02-24 16:20:59 +0700

    Add files via upload

---
+++ js/model.js
@@ -0,0 +1,16 @@
+class todo{
+	constructor(a, b, c, d, e, f){
+		this.id = a;
+		this.title = b;
+		this.matkul = c;
+		this.detail = d;
+		this.date = e;
+		this.status = f;
+	}
+}
+class matkul{
+	constructor(a, b){
+		this.idmatkul = a;
+		this.matkul = b;
+	}
+}
---
+++ js/setting.js
@@ -0,0 +1,10 @@
+const sett = {
+   // method display 
+   setdisplay(){
+      document.getElementById("settingssett").style.display = "block";
+   },
+   cleardisplay(){
+      document.getElementById("settingssett").style.display = "none";
+   },
+   // method event
+}
